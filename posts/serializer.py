3e6903a5881bc06c37143ae091f09467e22b94e5
javadocq <138106799+javadocq@users.noreply.github.com>
2024-12-16 17:49:25 +0900

    Delete posts directory

--- posts/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from posts.models import Posts
-
-
-class PostSerializer(ModelSerializer):
-    class Meta:
-        model = Posts
-        fields = ['id', 'title', 'content', 'author', 'create_at', 'update_at']
--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from posts import views
-
-
-urlpatterns = [
-    path('', views.PostList.as_view()),
-    # path('<int:id>/', views.PostDetail.as_view())
-]
