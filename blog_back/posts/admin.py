aaf356f21ff0eb4b865abb5fce1b4257692a34e4
javadocq <138106799+javadocq@users.noreply.github.com>
2025-02-09 01:40:33 +0900

    Delete blog_back directory

--- blog_back/posts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from posts.models import Posts
-
-# Register your models here.
-@admin.register(Posts)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "content", "author", "created_at", "updated_at")
